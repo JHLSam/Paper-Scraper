a96ac3c4ebcf9bbeb8280eb6725bb73de1989a4b
JHLSam <jhsamuqbsc@gmail.com>
2021-03-06 09:34:16 +0800

    Added screencapture feature and saved screenshot directory in preparation for OCR

---
+++ scrape.py
@@ -0,0 +1,97 @@
+import urllib.request
+import io
+from PIL import Image
+import pytesseract as pyt
+import wand
+from wand.image import Image as wi
+import cv2
+from screencap import *
+import os
+
+"""
+URL = input("Enter URL(e.g www.abc.com):" + " ")
+urltest = "https://www.google.com"
+DRIVER_PATH = "/Users/lionellim/desktop/webscrape/chromedriver"
+screencap(DRIVER_PATH,"https://" + URL,"screenshot.png")
+"""
+
+class OCR(object):
+    def __init__(self,URL,screenshot_name):
+        #Initialize with URL of webpage and chosen name of webpage screenshot file
+        self.URL = URL
+        self.screenshot_name = screenshot_name
+    """
+    def input_path(self, path):
+        self.path = input(path)
+        return self.path
+        """
+    
+    def url_to_png(self, DRIVER_PATH):
+        local_path = "/Users/lionellim/desktop/webscrape/chromedriver"
+        self.DRIVER_PATH = local_path
+        return screencap(self.DRIVER_PATH, "https://" + self.URL, self.screenshot_name)
+
+    def locate_screenshot_file(self):
+        if i in os.path().
+
+    def text_recognition(self):
+        """
+        Apply OCR to image file
+        """
+               
+    def text_search(self):
+        pass
+
+def getURL():
+    stream = urllib.request.urlopen(URL)
+    text = stream.read().decode('utf-8')
+    stream.close()
+    return text
+
+def getText():
+    """
+    Read & Return text in file
+
+    """
+    fd = open('test.html', 'r')
+    text = fd.read()
+    fd.close()
+    return text
+
+def find(substr, string, start):
+    """Return the position of the first substr in string, starting from start.
+    Return None if not found
+
+    Parameters:
+        substr(str): substring being searched for
+        string(str): string being searched
+        start(str): index where the search begines
+
+    Return:
+        int: index where substr is found within string
+    """
+    pos = start
+    size = len(substr)
+    while pos < len(string):
+        if substr == string[pos:pos+size]:
+            return pos
+        pos += 1
+    return None
+
+"""
+#text = getText()
+text2 = getURL()
+heading_index = find("E-commerce training site", text2, 0)
+if heading_index is None:
+    print("Heading not found")
+else:
+    room_index = find("Room:", text2, heading_index)
+    if room_index is None:
+        print("Room not found")
+    else:
+        tag_index = find("<", text2, room_index)
+        if tag_index is None:
+            print("tag not found")
+        else:
+            print(text2[room_index+6:tag_index])
+"""
---
+++ screencap.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+import selenium
+import os
+
+"""
+For ignoring unsecure connections
+custom = webdriver.ChromeOptions()
+custom.add_argument('--ignore-ssl-errors=yes')
+custom.add_argument('--ignore-certificate-errors')
+"""
+class CustomFileError(RuntimeError):
+    def __init__(self,existing_dir):
+        self.existing_dir = existing_dir
+
+def screencap(DRIVER_PATH,url,screenshot_name):
+    #DRIVER_PATH = "/Users/lionellim/desktop/webscrape/chromedriver"
+    driver = webdriver.Chrome(DRIVER_PATH)#(DRIVER_PATH,options = custom) for SSL certificate ignore 
+    driver.get(url)
+    driver.execute_script('document.body.style.zoom = "50%"')
+    storage_folder = "Screenshots_storage"
+    for root,dirs,files in os.walk(os.getcwd(), topdown=True):
+        for files in dirs:
+            print(files)
+            print(type(files))
+            if files != storage_folder:
+                os.mkdir(storage_folder)
+            else:
+                try:
+                    os.mkdir(storage_folder)
+                except FileExistsError:
+                    print("Folder already exists! Directory not created")
+                screenshot = driver.save_screenshot("Screenshots_storage/" + screenshot_name)
+                return driver.quit()
+
+                """
+            try:
+                os.mkdir(storage_folder)
+            except FileExistsError as e:
+                    print("Folder already exists, directory not created")
+                    
+    screenshot = driver.save_screenshot("Screenshots_storage/" + screenshot_name)
+    return driver.quit()
+                """
+
+
+DRIVER_PATH = "/Users/lionellim/desktop/webscrape/chromedriver"
+screencap(DRIVER_PATH,"https://www.yahoo.com","screenshots.png")
